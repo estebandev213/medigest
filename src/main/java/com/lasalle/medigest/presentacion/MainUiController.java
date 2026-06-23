dee3054d4110c1e1d98d1578020dac984d5f0f5b
lnxjala <jlazarinosa@gmail.com>
2026-06-23 00:00:30 -0500

    Rama Frontend y organizacion de carpetas

---
+++ src/main/java/com/lasalle/medigest/presentacion/MainUiController.java
@@ -0,0 +1,26 @@
+package com.lasalle.medigest.presentacion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainUiController {
+
+    // Redirige la raíz (http://localhost:8080/) directo al login
+    @GetMapping("/")
+    public String index() {
+        return "login";
+    }
+
+    // Ruta para el Login (templates/login.html)
+    // @GetMapping("/login")
+    // public String login() {
+    //     return "login";
+    // }
+
+    // Ruta para el Dashboard (templates/dashboard.html)
+    // @GetMapping("/dashboard")
+    // public String dashboard() {
+    //     return "dashboard";
+    // }
+}
